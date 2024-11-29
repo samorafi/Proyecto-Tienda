b18b873dc164533d95d46a8edde789e3e01307be
samorafi <sebasmora172522@gmail.com>
2024-11-28 21:02:44 -0600

    Commit semana 11

---
+++ dev/Tienda/src/main/java/com/tienda/dao/UsuarioDao.java
@@ -0,0 +1,15 @@
+package com.tienda.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tienda.domain.Usuario;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
+    Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+}
--- dev/Tienda/src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
+++ dev/Tienda/src/main/java/com/tienda/service/impl/ProductoServiceImpl.java
@@ -10,35 +10,54 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductoServiceImpl implements ProductoService {
-    
+
     @Autowired
     private ProductoDao productoDao;
 
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Producto> getProductos(boolean activos) {
-        var lista=productoDao.findAll();
+        var lista = productoDao.findAll();
         if (activos) {
-           lista.removeIf(e -> !e.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
     }
-    
+
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public Producto getProducto(Producto producto) {
         return productoDao.findById(producto.getIdProducto()).orElse(null);
     }
-    
+
     @Override
     @Transactional
-    public void save(Producto producto){
+    public void save(Producto producto) {
         productoDao.save(producto);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+    }
+
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
     }
     
     @Override
-    @Transactional
-    public void delete(Producto producto){
-        productoDao.delete(producto);
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
     }
 }
