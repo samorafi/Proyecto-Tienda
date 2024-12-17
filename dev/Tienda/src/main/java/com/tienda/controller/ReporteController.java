a81d9b85e80808509ae328fc1f795c86c6a32488
samorafi <sebasmora172522@gmail.com>
2024-12-17 16:40:48 -0600

    Reportes extra de practica

--- dev/Tienda/src/main/java/com/tienda/controller/ReporteController.java
+++ dev/Tienda/src/main/java/com/tienda/controller/ReporteController.java
@@ -2,6 +2,8 @@ package com.tienda.controller;
 
 import com.tienda.service.ReporteService;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +16,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/reportes")
 public class ReporteController {
-    
+
     @Autowired
     ReporteService reporteService;
-    
+
     @GetMapping("/principal")
-    public String principal (Model model){
+    public String principal(Model model) {
         return "/reportes/principal";
     }
-    
+
     @GetMapping("/usuarios")
     public ResponseEntity<Resource> usuarios(@RequestParam String tipo) throws IOException {
 
@@ -33,5 +35,25 @@ public class ReporteController {
     public ResponseEntity<Resource> ventas(@RequestParam String tipo) throws IOException {
 
         return reporteService.generaReporte("ventas", null, tipo);
+    }
+
+    @GetMapping("/facturas")
+    public ResponseEntity<Resource> facturas(@RequestParam String tipo) throws IOException {
+
+        return reporteService.generaReporte("facturas", null, tipo);
+    }
+
+    @GetMapping("/inventario")
+    public ResponseEntity<Resource> inventario(@RequestParam String tipo) throws IOException {
+
+        return reporteService.generaReporte("inventario", null, tipo);
+    }
+
+    @GetMapping("/facturaPorId")
+    public ResponseEntity<Resource> facturaPorId(@RequestParam Long facturaId, @RequestParam String tipo) throws IOException {
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("facturaId", facturaId);
+
+        return reporteService.generaReporte("facturas", parametros, tipo);
     }
 }
